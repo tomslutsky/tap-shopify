488741b6af3164fdaa2d0ad2f5d8e1cc39ac179b
Tom Slutsky <tom.slutsky@gmail.com>
2021-03-30 11:17:29 +0300

    since id smart collections

--- tap_shopify/streams/smart_collections.py
+++ tap_shopify/streams/smart_collections.py
@@ -7,7 +7,6 @@ from tap_shopify.context import Context
 class SmartCollections(Stream):
     name = 'smart_collections'
     replication_object = shopify.SmartCollection
-    replication_key = 'updated_at'
 
 
 Context.stream_objects['smart_collections'] = SmartCollections
